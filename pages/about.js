4207f6a90d6ef12c617715bfeeff7c3fcfa38d83
Your Name <you@example.com>
2019-04-10 13:22:02 -0300

    test

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,12 @@
-import Cualquiera from "../components/cualquiera";
+import Cualquiera from '../components/cualquiera';
+import '../components/style.scss';
 
 function About() {
-    return (
-      <h1>
-            About <Cualquiera/>
-      </h1>
-    );
+  return (
+    <h1>
+      About <Cualquiera />
+    </h1>
+  );
 }
 
 export default About;
