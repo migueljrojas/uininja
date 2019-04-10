d6a3893c6c977506eb4df68e294ff6cbb38d1ef0
Your Name <you@example.com>
2019-04-10 15:23:19 -0300

    inital set up

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,39 @@
-function Home() {
-  return <h1>Hello World</h1>;
+import React, { Component } from 'react';
+
+class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Cesar'
+    };
+
+    this.changeName = this.changeName.bind(this);
+  }
+
+  changeName(name) {
+    this.setState({
+      name
+    });
+  }
+
+  render() {
+    const { name } = this.state;
+
+    return (
+      <div>
+        <h1>Hello {name}</h1>
+        <button
+          onClick={() => {
+            this.changeName('Miguel');
+          }}
+          type="submit"
+        >
+          Cambiar Nombre
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Home;
