0ae88d51e16ca800ef7f615bf54c6ca24a0b9e70
Mike <migueljrojas@gmail.com>
2019-04-16 14:29:46 -0300

    button type

--- components/pages/home/hero.js
+++ components/pages/home/hero.js
@@ -24,7 +24,7 @@ const HeroSection = props => {
         onClick={() => {
           changeName('Miguel');
         }}
-        type="submit"
+        type="button"
       >
         Cambiar Nombre
       </Button>
