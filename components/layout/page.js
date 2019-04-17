1d4d2ca209e942b549d9f61d3b09ee14cf5f74e6
Mike <migueljrojas@gmail.com>
2019-04-17 00:03:36 -0300

    Added themProvider to the app

--- components/layout/page.js
+++ components/layout/page.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { ThemeProvider } from 'styled-components';
 import UIHead from './head';
 import UIHeader from './header';
 import UIFooter from './footer';
+import getCurrentTheme from '../utils/theme-manager';
 
 class UIPage extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      theme: {}
+    };
+  }
+
+  componentWillMount() {
+    const currentTheme = getCurrentTheme();
+
+    this.setState({
+      theme: currentTheme
+    });
   }
 
   render() {
     const { children } = this.props;
+    const { theme } = this.state;
 
     return (
-      <div>
-        <UIHead title="UI Ninja - Test Title" />
-        <UIHeader />
-        <main>{children}</main>
-        <UIFooter />
-      </div>
+      <ThemeProvider theme={theme}>
+        <div>
+          <UIHead title="UI Ninja - Test Title" />
+          <UIHeader />
+          <main>{children}</main>
+          <UIFooter />
+        </div>
+      </ThemeProvider>
     );
   }
 }
--- components/pages/home/hero.js
+++ components/pages/home/hero.js
@@ -11,7 +11,7 @@ const Button = styled.button`
   color: #fff;
 `;
 const Title = styled.h1`
-  background: red;
+  background: ${props => props.theme.mainColor};
   color: white;
 `;
 
